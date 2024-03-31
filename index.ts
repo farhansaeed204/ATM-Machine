47fb182ed89e004a0596bfa72280809d8058526d
Farhan_Saeed <128322226+farhansaeed204@users.noreply.github.com>
2024-03-31 22:00:05 +0500

    add new option

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 let pinCode = 1234;
 let Balance = 10000;
 
-console.log("Welcome to Farhan Saeed ATM");
+console.log(chalk.blue("\n \tWelcome to Farhan Saeed ATM\n"));
 
 let pinAnswer = await inquirer.prompt([
   {
     name: "pin",
     type: "number",
-    message: "Enter your pin",
+    message: chalk.yellow("Enter your pin"),
   },
 ]);
 
 if (pinAnswer.pin === pinCode) {
-  console.log("Your Pin is Currect, Successfully Login");
+  console.log(chalk.green("\nYour Pin is Currect, Successfully Login\n"));
 
   let operationAns = await inquirer.prompt([
     {
@@ -27,24 +28,50 @@ if (pinAnswer.pin === pinCode) {
   ]);
 
   if (operationAns.operation === "Withdraw Amount") {
-    let AmountAns = await inquirer.prompt([
+    let WithdrawAnswer = await inquirer.prompt([
       {
-        name: "amount",
-        type: "number",
-        message: "Enter your Amount",
+        name: "withdrawMethod",
+        type: "list",
+        message: "Select a withdraw method:",
+        choices: ["Fast Cash", "Enter Amount"],
       },
     ]);
+    if (WithdrawAnswer.withdrawMethod === "Fast Cash") {
+      let fastCashAns = await inquirer.prompt([
+        {
+          name: "fastCash",
+          type: "list",
+          message: "Select Amount:",
+          choices: [1000, 2000, 5000, 10000, 20000],
+        },
+      ]);
+      if (fastCashAns.fastCash > Balance) {
+        console.log(chalk.red("Insufficient Balance"));
+      } else {
+        Balance -= fastCashAns.fastCash;
+        console.log(chalk.green(`${fastCashAns.fastCash} Withdraw Successfully`));
+        console.log(chalk.blue(`Your Remaining Balance is ${Balance}`));
+      }
+    } else if (WithdrawAnswer.withdrawMethod === "Enter Amount") {
+      let AmountAns = await inquirer.prompt([
+        {
+          name: "amount",
+          type: "number",
+          message: "Enter your Amount",
+        },
+      ]);
 
-    if (AmountAns.amount > Balance) {
-      console.log("Insefficient Balance");
-    } else {
-      Balance -= AmountAns.amount;
-      console.log(`${AmountAns.amount} Successfully Withdraw`);
-      console.log(`Your Remaining Balance is ${Balance}`);
+      if (AmountAns.amount > Balance) {
+        console.log(chalk.red("Insefficient Balance"));
+      } else {
+        Balance -= AmountAns.amount;
+        console.log(chalk.green(`${AmountAns.amount} Successfully Withdraw`));
+        console.log(chalk.blue(`Your Remaining Balance is ${Balance}`));
+      }
     }
   } else if (operationAns.operation === "Check Amount") {
-    console.log(`Your Account Balance is ${Balance}`);
+    console.log(chalk.blue(`Your Account Balance is ${Balance}`));
   }
 } else {
-  console.log("Incorrect Password Please Try Again");
+  console.log(chalk.red("Incorrect Password Please Try Again"));
 }
